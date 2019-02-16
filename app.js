a51b192413c901a6277848d0d24ca132846eaf90
Eryn <johnmary1010@gmail.com>
2019-02-16 06:56:35 -0600

    update views

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const expresshbs = require('express-handlebars');
+const path = require('path');
+
+
+const app = express();
+
+
+//vew engine setup
+app.engine('handlebars', expresshbs());
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/message_sent', (req, res) => {
+  res.render('messageSent');
+});
+
+app.post('/send', (req, res) => {
+  // using SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    to: 'johnmary1010@gmail.com',
+    from: 'no_replay@erynmonestero.com',
+    subject: 'It sent',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    templateId: 'd-b11508eedb7a4c36af5a07e67ab5a691',
+    dynamic_template_data: {
+      nameName: req.body.name,
+      emailName: req.body.email,
+      companyName: req.body.company,
+      messageName: req.body.message,
+    },
+  };
+  sgMail.send(msg).then(() => {
+
+    console.log("message sent");
+    res.render('messageSent');
+  }).catch(error => {
+    console.error(error);
+  });
+});
+
+
+
+app.listen(3000, () => console.log("server running"));
+
---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.portfolio-link').on('click', function(){
+        $('.flip-box-back').css('opacity', '1');
+        $('.flip-box-inner').css('transform', 'rotateX(180deg)');
+    });
+    $('.about-link').on('click', function(){
+        $('.flip-box-back').css('opacity', '1');
+        $('.flip-box-inner').css('transform', 'rotateX(360deg)');
+     
+    });
+    $('.contact-link').on('click', function(){
+        $('.flip-box-inner').css('transform', 'rotateX(-90deg)');
+        $('.flip-box-back').delay('1000')
+        .queue(function (next) { 
+    $(this).css('opacity', '0') 
+    next(); 
+  });
+    });
+});
